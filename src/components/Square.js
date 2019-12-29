0192d6ee0e7cdc62dd4ab59c0f17f782c349e979
ali1129js <ali1129js@gmail.com>
2019-12-29 15:34:13 +0100

    win logic works

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,7 @@
  * @Author: Ali
  * @Date:   2019-12-26T22:22:35+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-12-29T15:29:19+01:00
+ * @Last modified time: 2019-12-29T15:32:44+01:00
  */
 import React from 'react'
 
@@ -17,8 +17,8 @@ const Square = ({onClick,value}) => (
 const sqaureStyle = {
   width:"150px",
   height:"150px",
-  background:"blue",
+  background:"skyblue",
   fontSize:"3rem",
-  boxShadow:"10px 10px 5px #000"
+  boxShadow:"10px 10px 5px #030"
 }
 export default Square
